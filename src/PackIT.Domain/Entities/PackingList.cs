d7f7f2071d8451a30143509958454de618378ed9
brSaif <saifeddine.benromdhane@protonmail.com>
2022-04-01 23:48:53 +0100

    Add DDD Entities

---
+++ src/PackIT.Domain/Entities/PackingList.cs
@@ -0,0 +1,23 @@
+﻿using PackIT.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Domain.Entities
+{
+    public class PackingList
+    {
+        public Guid Id { get; private set; }
+        private PackingListName _name;
+        private Localization _localization;
+
+        internal PackingList(Guid id, PackingListName name, Localization localization)
+        {
+            Id = id;
+            _name = name;
+            _localization = localization;
+        }
+    }
+}
---
+++ src/Shared/PackIT.Shared.Abstraction/Exceptions/PackITException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Shared.Abstraction.Exceptions
+{
+    public abstract class PackITException : Exception
+    {
+        protected PackITException(string exception) : base(exception) { }
+    }
+}
