9d43a39627367d665607dd441adbebd6c7d44c65
brSaif <saifeddine.benromdhane@protonmail.com>
2022-04-02 13:17:15 +0100

    Setup Factories

---
+++ src/PackIT.Domain/Factories/PackingListFactory.cs
@@ -0,0 +1,16 @@
+﻿using PackIT.Domain.Entities;
+using PackIT.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Domain.Factories
+{
+    public class PackingListFactory : IPackingListFactory
+    {
+        PackingList Create(PackingListId id, PackingListName name, Localization localization);
+        PackingList CreateWithDefaultItems(PackingListId id, )
+    }
+}
---
+++ src/PackIT.Domain/ValueObjects/Temperature.cs
@@ -0,0 +1,29 @@
+﻿using PackIT.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Domain.ValueObjects
+{
+    public record Temperature
+    {
+
+        public double Value { get; }
+
+        public Temperature(double value)
+        {
+            if (value is < -5 or > 50)
+                throw new InvalidTemperatureException(value);
+            Value = value  ;
+        }
+
+        public static implicit operator double (Temperature temp)
+            => temp.Value;
+
+        public static implicit operator Temperature(double temp)
+            => new(temp);
+
+    }
+}
