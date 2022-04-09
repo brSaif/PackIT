ecb30f04577fa433b1c49e523835402097814e9a
brSaif <saifeddine.benromdhane@protonmail.com>
2022-04-09 16:55:24 +0100

    Setup controllers

---
+++ src/PackIT.Api/Controllers/PackingListsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PackIT.Application.Commands;
+using PackIT.Application.DTO;
+using PackIT.Application.Queries;
+using PackIT.Shared.Abstraction.Commands;
+using PackIT.Shared.Abstraction.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Api.Controllers
+{
+    public class PackingListsController : BaseController
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public PackingListsController(IQueryDispatcher query,
+                                      ICommandDispatcher command)
+        {
+            _queryDispatcher = query;
+            _commandDispatcher = command;
+        }
+
+        #region Queries
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<PackingListDto>> Get([FromRoute] GetPackingList query)
+        {
+            var result = await _queryDispatcher.QueryDispatcher(query);
+            return OkOrNotFound(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PackingListDto>>> Get([FromQuery] SearchPackingLists query)
+        {
+            var results = await _queryDispatcher.QueryDispatcher(query);
+            return OkOrNotFound(results);
+        }
+
+        #endregion
+
+
+        #region Commands
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreatingPackingListWithItems command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return CreatedAtAction(nameof(Get), new { id = command.Id }, null);
+        }
+
+        [HttpPut("{PackingListId}/items")]
+        public async Task<IActionResult> Put([FromBody] AddPackingItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpPut("{PackingListId:guid}/items/{name}/pack")]
+        public async Task<IActionResult> Put([FromBody] PackItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] RemovePackingItem command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] RemovePackingList command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        } 
+        #endregion
+    }
+}
